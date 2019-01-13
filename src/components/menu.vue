bb0c883c8d4b61d01c3213c054cc3e4b30415d03
liuxuewen <457460089@qq.com>
2019-01-14 00:38:50 +0800

    'shouye'

---
+++ src/components/menu.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="menu">
+    <ul>
+      <li>
+        <span class="icon"></span>
+        <p>推荐</p>
+      </li>
+      <li>
+        <span class="icon"></span>
+        <p>我的</p>
+      </li>
+      <li>
+        <span class="icon"></span>
+        <p>设置</p>
+      </li>
+    </ul>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  // props: [],
+  data () {
+    return {
+      // width: this.width,
+      // text: this.text
+    }
+  }
+}
+</script>
+
+<style scoped>
+.menu{
+  width: 100%;
+  height: 103rpx;
+  box-sizing: border-box;
+  padding: 14rpx 50rpx 4rpx;
+}
+.menu ul{
+  display: flex;
+}
+.menu li{
+  flex: 33%;
+  text-align: center;
+}
+.menu p{
+  height: 38rpx;
+  line-height: 38rpx;
+  font-size: 18rpx;
+}
+.menu span{
+  display: inline-block;
+}
+.menu span:nth-child(1){
+  width: 50rpx;
+  height: 43rpx;
+  background: url(../../static/image/icon/i-recom-g.jpg) no-repeat;
+  background-size: 100% 100%;
+}
+</style>
---
+++ src/components/smallImgItem.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="smallImage">
+    <div class="lift">
+      <span>{{text}}p</span>
+      <img :src="imgSrc" alt="">
+    </div>
+    <div class="right">
+      <h3>{{tit}}</h3>
+      <p>#{{describe}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['text','imgSrc','tit','describe'],
+  data () {
+    return {
+      // width: this.width,
+      // text: this.text
+    }
+  }
+}
+</script>
+
+<style scoped>
+img{
+  display: block;
+}
+.smallImage {
+  box-sizing: border-box;
+  padding: 10rpx;
+  width: 100%;
+  box-shadow:  0rpx 5rpx 10rpx #dfdfdf;
+}
+.lift{
+  float: left;
+  position: relative;
+}
+.lift img{
+  width: 300rpx;
+  height: 180rpx;
+}
+.lift span{
+  position: absolute;
+  right: 10rpx;
+  top: -10rpx;
+  height: 40rpx;
+  line-height: 40rpx;
+  width: 40rpx;
+  text-align: center;
+  background-color: #535d84;
+  color: #fff;
+  font-size: 20rpx;
+}
+.right{
+  margin-left: 327rpx;
+  box-sizing: border-box;
+  padding-top: 64rpx;
+}
+.right h3{
+  height: 32rpx;
+  line-height: 32rpx;
+  color: #000;
+  margin-bottom: 64rpx;
+}
+.right p{
+  font-size: 20rpx;
+  color: #999;
+}
+</style>
